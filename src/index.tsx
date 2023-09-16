f1b4936c436a93975f096e21585747970d4c9595
danil volobuyev <dani736@gmail.com>
2023-09-16 15:55:08 +0300

    push

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import Layout from './components/LayoutArea/Layout/Layout';
 import './index.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
